859b9a162822095b4aec1b7dfbb5109ed02c552b
mgalvc <matheusgcorreia@gmail.com>
2020-03-22 20:46:39 -0300

    implementing insertion in BaseModel

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -1,6 +1,7 @@
 <?php
 
 class BaseModel {
+    private $db;
     protected $table;
 
     protected $id;
@@ -10,10 +11,32 @@ class BaseModel {
     protected $deleted_at;
 
     protected function __construct($table) {
+        $this->db = new DB();
+
+        if(!$this->db->connected) {
+            die("Unable to connect to database");
+        }
+
         $this->table = $table;
     }
 
-    protected static function insert($data) {
-        echo "will save";
+    protected function insert($data) {
+        $fields = join(",", array_keys($data));
+        $values = array_values($data);
+        
+        foreach ($values as $key => $value) {
+            $values[$key] = "'$value'";
+        }
+
+        $values = join(",", $values);
+        
+        $sql = "INSERT INTO $this->table ($fields) VALUES ($values)";
+        $result = $this->db->exec($sql);
+
+        if(empty($result)) {
+            return ["success" => false, "error" => $this->db->get_error()];
+        }
+
+        return ["success" => true, "inserted_id" => $this->db->get_inserted_id()];
     }
 }
