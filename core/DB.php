048d9f1a7c71e8b7689ca81245ca12215cbc9736
mgalvc <matheusgcorreia@gmail.com>
2020-03-22 20:46:02 -0300

    implementing db class

--- core/DB.php
+++ core/DB.php
@@ -10,10 +10,27 @@ class DB {
     public $connected = true;
 
     public function __construct() {
-        $this->conn = new mysqli(self::SERVER, self::USERNAME, self::PASSWORD);
+        $this->conn = new mysqli(self::SERVER, self::USERNAME, self::PASSWORD, self::DATABASE);
 
         if($this->conn->connect_error) {
             $this->connected = false;
         }
+    }
+
+    public function exec($sql) {
+        return mysqli_query($this->conn, $sql);
+        // $response = [];
+        // while($row = mysqli_fetch_assoc($result)) {
+        //     array_push($response, $row);
+        // }
+        // return $response;
+    }
+
+    public function get_error() {
+        return $this->conn->error;
+    }
+
+    public function get_inserted_id() {
+        return $this->conn->insert_id;
     }
 }
