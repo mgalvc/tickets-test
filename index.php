ed0de3f2f83d74e4a563e1280ce61d4341baaed8
mgalvc <matheusgcorreia@gmail.com>
2020-03-22 20:45:43 -0300

    removing db connection from index

--- index.php
+++ index.php
@@ -2,14 +2,11 @@
 
 require("core/DB.php");
 require("core/Router.php");
-require("models/User.php");
 
 $router = new Router();
-$db = new DB();
 
-if(!$db->connected) {
-    die("Unable to connect to database");
-}
+require("models/BaseModel.php");
+require("models/User.php");
 
 require("routes/users.php");
 require("routes/tickets.php");
