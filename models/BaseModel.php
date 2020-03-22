3781b58719efee4f780f33662f2b5a9656be610c
mgalvc <matheusgcorreia@gmail.com>
2020-03-22 17:47:49 -0300

    implementing base model class

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -1,9 +1,19 @@
 <?php
 
 class BaseModel {
-    private $table;
+    protected $table;
 
-    public function __construct($table) {
+    protected $id;
+
+    protected $created_at;
+    protected $updated_at;
+    protected $deleted_at;
+
+    protected function __construct($table) {
         $this->table = $table;
+    }
+
+    protected static function insert($data) {
+        echo "will save";
     }
 }
