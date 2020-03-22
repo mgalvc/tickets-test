922b38415c7fd0cdeaeb46d33c076720869668f6
mgalvc <matheusgcorreia@gmail.com>
2020-03-22 17:48:02 -0300

    implementing user class

--- models/User.php
+++ models/User.php
@@ -1,7 +1,51 @@
 <?php 
 
-include("./BaseModel.php");
+include("BaseModel.php");
 
 class User extends BaseModel {
+    private $login;
+    private $password;
+    private $public_name;
 
+    public function __construct($data) {
+        $this->login = $data["login"];
+        $this->password = $data["password"];
+        $this->public_name = $data["public_name"];
+
+        parent::__construct("users");
+    }
+
+    public function save() {
+        BaseModel::insert(["algo"]);
+    }
+
+    public static function validate($data) {
+        $response = [];
+        $success = true;
+
+        if(empty($data["login"])) {
+            $response["login"] = "required";
+        } else if(User::already_exists($data["login"])) {
+            $response["login"] = "unavailable";
+        }
+
+        if(empty($data["password"])) {
+            $response["password"] = "required";
+        }
+
+        if(empty($data["public_name"])) {
+            $response["public_name"] = "required";
+        }
+
+        if(!empty($response)) {
+            $success = false;
+        }
+
+        $response["success"] = $success;
+        return $response;
+    }
+
+    private static function already_exists($login) {
+
+    }
 }
