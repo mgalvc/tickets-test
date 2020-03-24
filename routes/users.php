6d4c5fd83ff10471837d6b061407f279f914bdc6
mgalvc <matheusgcorreia@gmail.com>
2020-03-23 23:49:16 -0300

    implementando GET e DELETE na rota de user

--- routes/users.php
+++ routes/users.php
@@ -1,7 +1,11 @@
 <?php
 
-$router->on("GET", "users", function() {
-    echo "GET request to users";
+$router->on("GET", "users", function($body) {
+    $users = new User();
+    if(!empty($body["id"])) {
+        Router::send($users->get($body["id"]));
+    }
+    Router::send($users->get());
 });
 
 $router->on("POST", "users", function($body) {
@@ -19,6 +23,13 @@ $router->on("PUT", "users", function() {
     echo "PUT request to users";
 });
 
-$router->on("DELETE", "users", function() {
-    echo "DELETE request to users";
+$router->on("DELETE", "users", function($body) {
+    if(empty($body["id"])) {
+        Router::send([
+            "success" => false,
+            "error" => "id must be provided"
+        ]);
+    }
+    $user = new User();
+    Router::send($user->remove($body["id"]));
 });
