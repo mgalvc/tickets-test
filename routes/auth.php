3ad907adaa981bebbdbe0f5bbb323e734d97172e
mgalvc <matheusgcorreia@gmail.com>
2020-03-25 23:57:31 -0300

    implementing auth routes

--- routes/auth.php
+++ routes/auth.php
@@ -1,9 +1,33 @@
 <?php
 
-$router->on("POST", "auth", function() {
-    echo "POST request to auth - login";
+$router->on("POST", "auth", function($body) {
+    if(empty($body["login"])) {
+        Router::send([
+            "success" => false,
+            "error" => "login must be provided"
+        ]);
+    }
+
+    if(empty($body["password"])) {
+        Router::send([
+            "success" => false,
+            "error" => "password must be provided"
+        ]);
+    }
+
+    $auth = new Token();
+    $token = $auth->generate_token($body["login"], $body["password"]);
+    Router::send($token);
 });
 
-$router->on("DELETE", "auth", function() {
-    echo "DELETE request to auth - logout";
+$router->on("DELETE", "auth", function($body) {
+    if(empty($body["token"])) {
+        Router::send([
+            "success" => false,
+            "error" => "token must be provided"
+        ]);
+    }
+
+    $token = new Token();
+    Router::send($token->remove($body["token"]));
 });
